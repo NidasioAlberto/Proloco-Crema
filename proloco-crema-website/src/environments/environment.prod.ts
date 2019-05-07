159334aa2cd92d370e942b13d1929d04667e26af
NidasioAlberto <nidasioalberto@gmail.com>
2019-05-07 12:40:55 +0200

    Updated api keys

--- proloco-crema-website/src/environments/environment.prod.ts
+++ proloco-crema-website/src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCvZbHqwohcCGjLj58dEV_BoZmFOdwlRGY",
+    apiKey: "AIzaSyCK7FViBJ3b51ILV2eOxJjKFq373CgqdDs",
     authDomain: "proloco-crema.firebaseapp.com",
     databaseURL: "https://proloco-crema.firebaseio.com",
     projectId: "proloco-crema",
     storageBucket: "proloco-crema.appspot.com",
-    messagingSenderId: "405138713326"
+    messagingSenderId: "405138713326",
+    appId: "1:405138713326:web:74dbc00be545e7e9"
   }
 };
--- proloco-crema-website/src/environments/environment.ts
+++ proloco-crema-website/src/environments/environment.ts
@@ -5,12 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCvZbHqwohcCGjLj58dEV_BoZmFOdwlRGY",
+    apiKey: "AIzaSyCK7FViBJ3b51ILV2eOxJjKFq373CgqdDs",
     authDomain: "proloco-crema.firebaseapp.com",
     databaseURL: "https://proloco-crema.firebaseio.com",
     projectId: "proloco-crema",
     storageBucket: "proloco-crema.appspot.com",
-    messagingSenderId: "405138713326"
+    messagingSenderId: "405138713326",
+    appId: "1:405138713326:web:74dbc00be545e7e9"
   }
 };
 
